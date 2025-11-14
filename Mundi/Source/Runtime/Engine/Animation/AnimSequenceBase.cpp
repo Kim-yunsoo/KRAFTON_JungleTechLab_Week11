f8859b25a0fb772a4cdeb1f2ed11e98de1a604ec
YEONGBIN PAK <suplem07@naver.com>
2025-11-15 00:42:44 +0900

    feat: AnimSequenceBase 클래스 정의
    1. AnimSequenceBase 클래스의 구조 및 역할을 정의하였습니다.
    모든 종류의 애니메이션은 해당 클래스를 상속 받아 파생 클래스로 정의가 되어야만 합니다.
    모든 애니메이션은 재생, 트리거 등의 공통 속성을 가지고 있고, 해당 클래스가 이 역할을 맡습니다.

--- Mundi/Source/Runtime/Engine/Animation/AnimSequenceBase.cpp
+++ Mundi/Source/Runtime/Engine/Animation/AnimSequenceBase.cpp
@@ -1,2 +1,106 @@
 ﻿#include "pch.h"
 #include "AnimSequenceBase.h"
+#include "JsonSerializer.h"
+
+void UAnimSequenceBase::AddNotify(const FAnimNotifyEvent& Notify)
+{
+    Notifies.Add(Notify);
+    SortNotifies();
+}
+
+void UAnimSequenceBase::SetNotifies(const TArray<FAnimNotifyEvent>& InNotifies)
+{
+    Notifies = InNotifies;
+    SortNotifies();
+}
+
+void UAnimSequenceBase::RemoveNotifiesByName(const FName& InName)
+{
+    if (Notifies.empty()) { return; }
+
+    Notifies.erase(std::remove_if(Notifies.begin(),Notifies.end(),
+        [&InName](const FAnimNotifyEvent& Notify)
+        {
+            return Notify.NotifyName == InName;
+        }),
+        Notifies.end());
+}
+
+void UAnimSequenceBase::GetAnimNotifiesInRange(float StartTime, float EndTime, TArray<FAnimNotifyEvent>& OutNotifies) const
+{
+    OutNotifies.clear(); // 오염 방지
+
+    if (Notifies.empty()) { return; }
+
+    if (StartTime <= EndTime) // 정방향으로 진행하는 경우
+    {
+        for (const FAnimNotifyEvent& Notify : Notifies)
+        {
+            if (Notify.IsWithin(StartTime, EndTime)) { OutNotifies.Add(Notify); }
+        }
+    }
+    else if (StartTime > EndTime)  // 역방향으로 진행하는 경우
+    {
+        for (auto It = Notifies.rbegin(); It != Notifies.rend(); ++It)
+        {
+            if (It->IsWithin(EndTime, StartTime)) { OutNotifies.Add(*It); }
+        }
+    }
+}
+
+void UAnimSequenceBase::Serialize(const bool bInIsLoading, JSON& InOutHandle)
+{
+    Super::Serialize(bInIsLoading, InOutHandle);
+
+    if (bInIsLoading)
+    {
+        FJsonSerializer::ReadFloat(InOutHandle, "TotalPlayLength", TotalPlayLength, 0.f, false);
+        FJsonSerializer::ReadFloat(InOutHandle, "PlayRate", PlayRate, 1.f, false);
+        FJsonSerializer::ReadBool(InOutHandle, "bLoop", bLoop, false, false);
+
+        JSON NotifyArray;
+        if (FJsonSerializer::ReadArray(InOutHandle, "Notifies", NotifyArray, nullptr, false))
+        {
+            Notifies.clear();
+            for (size_t Idx = 0; Idx < NotifyArray.size(); ++Idx)
+            {
+                const JSON& NotifyJson = NotifyArray.at(static_cast<unsigned>(Idx));
+                if (NotifyJson.JSONType() != JSON::Class::Object) { continue; }
+
+                FAnimNotifyEvent Notify;
+                FJsonSerializer::ReadFloat(NotifyJson, "TriggerTime", Notify.TriggerTime, 0.f, false);
+                FJsonSerializer::ReadFloat(NotifyJson, "Duration", Notify.Duration, 0.f, false);
+
+                FString NotifyNameStr;
+                if (FJsonSerializer::ReadString(NotifyJson, "NotifyName", NotifyNameStr, "", false))
+                {
+                    Notify.NotifyName = FName(NotifyNameStr);
+                }
+
+                Notifies.Add(Notify);
+            }
+            SortNotifies();
+        }
+        else
+        {
+            Notifies.clear();
+        }
+    }
+    else
+    {
+        InOutHandle["TotalPlayLength"] = TotalPlayLength;
+        InOutHandle["PlayRate"] = PlayRate;
+        InOutHandle["bLoop"] = bLoop;
+
+        JSON NotifyArray = JSON::Make(JSON::Class::Array);
+        for (const FAnimNotifyEvent& Notify : Notifies)
+        {
+            JSON NotifyJson = JSON::Make(JSON::Class::Object);
+            NotifyJson["TriggerTime"] = Notify.TriggerTime;
+            NotifyJson["Duration"] = Notify.Duration;
+            NotifyJson["NotifyName"] = Notify.NotifyName.ToString().c_str();
+            NotifyArray.append(NotifyJson);
+        }
+        InOutHandle["Notifies"] = NotifyArray;
+    }
+}
